920464b3c327182a27bd1e8d194c6a41885b8d57
Charlotte Qazi <charlottemdavies@hotmail.co.uk>
2021-11-29 16:16:36 +0000

    added pressable interactivity

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 
 import React, {useState} from 'react';
-import { View, ScrollView, Text, TextInput } from 'react-native';
+import { View, ScrollView, Text, TextInput, Pressable, StyleSheet } from 'react-native';
 
 const App = () => {
     return(
@@ -13,6 +13,12 @@ const App = () => {
           <FormField label='favourite toy'/>
           <FormField label='password'/>
         </ScrollView>
+        <View style={styles.layout}>
+          <Pressable>
+            {(state) => <Box pressed={state.pressed} />}
+          </Pressable>
+        </View>
+        <StatusBar style='auto'/>
       </View>
     )
 };
@@ -20,7 +26,7 @@ const App = () => {
 export default App;
 
 const FormField = (props) => {
-  const [text, onChangeText] = React.useState("Please enter");
+  const [text, onChangeText] = useState("Please enter");
   const defaultlabel = `Please enter ${props.label}`;
 
   return (
@@ -31,8 +37,29 @@ const FormField = (props) => {
         onChangeText={onChangeText}   
         secureTextEntry={props.label === 'password' ? true : false}
         style={{padding: 8}}
-        ></TextInput>
-        <StatusBar style='auto'/>
+      />
     </View>
   )
 }
+
+export const Box = (props) => (
+  <View style={[
+    styles.box,
+    props.pressed && 
+      {backgroundColor: 'blue'}
+    ]} 
+  />
+);
+
+export const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
