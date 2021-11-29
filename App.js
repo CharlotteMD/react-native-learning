9ec30c7b31fa65b5d82cdba13e8b2837156c0fc2
Charlotte Qazi <charlottemdavies@hotmail.co.uk>
2021-11-29 13:37:43 +0000

    custom components

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
 
-const App = () => (
-  <View style={{ flex: 1, justifyContent: 'center' }}>
-    <Text style={{ fontSize: 24, textAlign: 'center' }}>
-      Scroll me!
-    </Text>
-    <View style={{ height: 400, backgroundColor: '#e5e5e5' }}>
-      <ScrollView horizontal>
-        <View style={{ width: 300, height: 300, backgroundColor: 'red' }} />
-        <View style={{ width: 300, height: 300, backgroundColor: 'green' }} />
-        <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
-      </ScrollView>
-    </View>
-    <StatusBar style="auto" />
-  </View>
-);
+import React, {useState} from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
+
+const App = () => {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 8}}>
+        <ScrollView style={{ backgroundColor: '#ecf0f1' }}>
+          <FormField label='name'/>
+          <FormField label='date of birth'/>
+          <FormField label='breed'/>
+          <FormField label='favourite toy'/>
+          <FormField label='password'/>
+        </ScrollView>
+      </View>
+    )
+};
 
 export default App;
+
+const FormField = (props) => {
+  const [text, onChangeText] = React.useState("Please enter");
+  const defaultlabel = `Please enter ${props.label}`;
+
+  return (
+    <View style={{padding: 16, fontSize: 18}}>
+      <Text style={{padding: 8}}>{defaultlabel}</Text>
+      <TextInput 
+        placeholder={`Please enter ${props.label}`} 
+        onChangeText={onChangeText}   
+        secureTextEntry={props.label === 'password' ? true : false}
+        style={{padding: 8}}
+        ></TextInput>
+        <StatusBar style='auto'/>
+    </View>
+  )
+}
